1bacedf29866a9ae8039c21a74392ece7cc8b435
nibchee <nirbhaybansal28@gmail.com>
2023-03-20 22:03:23 +0530

    Spring-Data-REST

--- src/main/java/com/geekynib/springboot/cruddemo/dao/EmployeeRepository.java
+++ src/main/java/com/geekynib/springboot/cruddemo/dao/EmployeeRepository.java
@@ -2,7 +2,10 @@ package com.geekynib.springboot.cruddemo.dao;
 
 import com.geekynib.springboot.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.webmvc.RepositoryRestController;
 
+@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee,Integer>{
 
     //thats it..no need to write any code LOL!
